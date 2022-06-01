f2ff220dcf0d3b3e72a3d7e8f2a5472f12073362
AndersPC <anders.wilhelm.gran@gmail.com>
2022-06-01 22:10:12 +0200

    minimal express server

--- index.jsx
+++ index.jsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom";
 import {useState} from "react";
+import {useEffect} from "react";
 
-const movies = [
+const MOVIES = [
     {
         title: "Cool ass movie",
         plot: "Cool shit happens",
@@ -21,12 +22,21 @@ function FrontPage() {
         <h1>Kristiania Movie Database</h1>
         <ul>
             <li><Link to="/movies">List movies</Link></li>
-            <li><Link to="/movie/new">New Movie</Link></li>
+            <li><Link to="/movies/new">New Movie</Link></li>
         </ul>
     </div>;
 }
 
-function ListMovies({movies}) {
+function ListMovies({moviesApi}) {
+    const [movies, setMovies] = useState();
+    useEffect(async () => {
+        setMovies(undefined);
+        setMovies(await moviesApi.listMovies());
+    }, []);
+
+    if (!movies) {
+        return <div>Loading...</div>
+    }
     return <div>
         <h1>List movies</h1>
         <ul>
@@ -40,23 +50,20 @@ function ListMovies({movies}) {
     </div>
 }
 
-function NewMovie({onAddMovie}) {
+function NewMovie({moviesApi}) {
     const [title, setTitle] = useState("Initial");
     const [year, setYear] = useState("Initial");
-    const [plot,setPlot] = useState("Initial");
+    const [plot, setPlot] = useState("Initial");
 
     const navigate = useNavigate();
 
-
-
-
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        onAddMovie({title, year, plot});
+        await moviesApi.onAddMovie({title, year, plot});
         navigate("/");
     }
     return <form onSubmit={handleSubmit}>
-        <h1>New movie</h1>;
+        <h1>New movie</h1>
         <div>
             <label>Title: <input value={title} onChange={e => setTitle(e.target.value)}/></label>
         </div>
@@ -66,18 +73,20 @@ function NewMovie({onAddMovie}) {
         <div>
             <label>Plot: <textarea value={plot} onChange={e => setPlot(e.target.value)}/></label>
         </div>
-        <pre>
-            {JSON.stringify({title, year, plot})}
-        </pre>
+        <button>Submit</button>
     </form>
 }
 
 function Application() {
+        const moviesApi = {
+            onAddMovie: async (m) => MOVIES.push(m),
+            listMovies: async () => MOVIES
+        }
         return <BrowserRouter>
             <Routes>
                 <Route path="/" element={<FrontPage />} />
-                <Route path="/movie/new" element={<NewMovie onAddMovie={m => movies.push(m)}/>} />
-                <Route path="/movies" element={<ListMovies movies={movies} />} />
+                <Route path="/movies/new" element={<NewMovie moviesApi={moviesApi}/>} />
+                <Route path="/movies" element={<ListMovies moviesApi={moviesApi}/>} />
             </Routes>
         </BrowserRouter>
 
