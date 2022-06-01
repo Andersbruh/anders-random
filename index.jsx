759bcc59553817bb3fb4c66cb1420e3f947a5d99
AndersPC <anders.wilhelm.gran@gmail.com>
2022-06-01 10:30:34 +0200

    <newMovie> crazy state

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import {useState} from "react";
 
 const movies = [
     {
@@ -39,11 +40,36 @@ function ListMovies({movies}) {
     </div>
 }
 
+function NewMovie() {
+    const [title, setTitle] = useState("Initial");
+    const [year, setYear] = useState("Initial");
+    const [plot,setPlot] = useState("Initial");
+    function handleSubmit(e) {
+        e.preventDefault();
+        movies.push({title, year, plot});
+    }
+    return <form>
+        <h1>New movie</h1>;
+        <div>
+            <label>Title: <input value={title} onChange={e => setTitle(e.target.value)}/></label>
+        </div>
+        <div>
+            <label>Year: <input value={year} onChange={e => setYear(e.target.value)}/></label>
+        </div>
+        <div>
+            <label>Plot: <textarea value={plot} onChange={e => setPlot(e.target.value)}/></label>
+        </div>
+        <pre>
+            {JSON.stringify({title, year, plot})}
+        </pre>
+    </form>
+}
+
 function Application() {
         return <BrowserRouter>
             <Routes>
                 <Route path="/" element={<FrontPage />} />
-                <Route path="/movie/new" element={<h1>New movie</h1>} />
+                <Route path="/movie/new" element={<NewMovie/>} />
                 <Route path="/movies" element={<ListMovies movies={movies} />} />
             </Routes>
         </BrowserRouter>
